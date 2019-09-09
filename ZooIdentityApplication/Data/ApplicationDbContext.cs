b108594cef05a9a1ad0138b99f1d1fb8e3d71b98
pvladimir <bbabbai@mail.ru>
2019-09-10 01:52:33 +0600

    reserve application

---
+++ ZooIdentityApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ZooIdentityApplication.Models;
+
+namespace ZooIdentityApplication.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User, Role, int>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ ZooIdentityApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ZooIdentityApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
